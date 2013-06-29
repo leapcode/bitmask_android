28dedb2fbe42303081ed201de7e33db6f8432297
Parménides GV <parmegv@sdf.org>
2013-06-29 13:04:45 +0200

    Substituted IOException by SSLHandshakeException.
    In getStringFromProvider, there is no IOException but an
    SSLHandshakeException. Substituing this gives our code more semantics.

--- src/se/leap/leapclient/ProviderAPI.java
+++ src/se/leap/leapclient/ProviderAPI.java
@@ -26,6 +26,7 @@ import java.util.Scanner;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.TrustManager;
