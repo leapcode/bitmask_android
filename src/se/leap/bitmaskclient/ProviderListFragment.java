e67c405e9a4e58cce55dac8c107e0b9010965f48
Parménides GV <parmegv@sdf.org>
2013-09-09 20:08:00 +0200

    Preseeded providers only have provider.json url
    I've refactored everything to be able to remove all but provider.json
    url at urls files.

--- src/se/leap/bitmaskclient/ProviderListFragment.java
+++ src/se/leap/bitmaskclient/ProviderListFragment.java
@@ -155,7 +155,7 @@ public class ProviderListFragment extends ListFragment {
 
         // Notify the active callbacks interface (the activity, if the
         // fragment is attached to one) that an item has been selected.
-        mCallbacks.onItemSelected(ProviderListContent.ITEMS.get(position).id);
+        mCallbacks.onItemSelected(ProviderListContent.ITEMS.get(position).name());
         
         for(int item_position = 0; item_position < listView.getCount(); item_position++) {
         	if(item_position != position)
