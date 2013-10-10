002e1f633b7bda7b713f84be3bf0ad19f5a23c8c
Parménides GV <parmegv@sdf.org>
2013-10-10 17:13:13 +0200

    Changed some documentation and scripts.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-android update project --path . --name "Bitmask Android" --target android-17
+android update project --path . --name "Bitmask for Android" --target android-17
 ant debug
