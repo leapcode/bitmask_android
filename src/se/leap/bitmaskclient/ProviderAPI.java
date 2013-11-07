cede0d3c59742b1f252aa59e72ea376f3ad9486d
Parménides GV <parmegv@sdf.org>
2013-11-07 16:32:11 +0100

    Logging out deletes the token from LeapSRPSession.

--- src/se/leap/bitmaskclient/ProviderAPI.java
+++ src/se/leap/bitmaskclient/ProviderAPI.java
@@ -678,6 +678,7 @@ public class ProviderAPI extends IntentService {
 
 			int responseCode = urlConnection.getResponseCode();
 			broadcast_progress(progress++);
+			LeapSRPSession.setToken("");
 			Log.d("logout", Integer.toString(responseCode));
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
