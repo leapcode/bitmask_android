6dfb02da9cd6c28b8a3016cf78454add6408c13d
Parménides GV <parmegv@sdf.org>
2013-12-03 22:26:31 +0100

    Vpn profiles get updated after switching provider.
    parsedEipSerial wasn't being reset after switching provider, so updating gateways thought there was no real update.
    
    After switching provider, we also remove all other vpn profiles.

--- src/se/leap/bitmaskclient/Dashboard.java
+++ src/se/leap/bitmaskclient/Dashboard.java
@@ -106,7 +106,7 @@ public class Dashboard extends Activity implements LogInDialog.LogInDialogInterf
 	    
 		ConfigHelper.setSharedPreferences(getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE));
 		preferences = ConfigHelper.shared_preferences;
-		
+
 		authed_eip = ConfigHelper.getBoolFromSharedPref(EIP.AUTHED_EIP);
 		if (ConfigHelper.getStringFromSharedPref(Provider.KEY).isEmpty())
 			startActivityForResult(new Intent(this,ConfigurationWizard.class),CONFIGURE_LEAP);
@@ -125,14 +125,15 @@ public class Dashboard extends Activity implements LogInDialog.LogInDialogInterf
 		if ( requestCode == CONFIGURE_LEAP || requestCode == SWITCH_PROVIDER) {
 		// It should be equivalent: if ( (requestCode == CONFIGURE_LEAP) || (data!= null && data.hasExtra(STOP_FIRST))) {
 			if ( resultCode == RESULT_OK ){
-				ConfigHelper.saveSharedPref(EIP.AUTHED_EIP, authed_eip);
 				if ((data != null) && (data.hasExtra(STOP_FIRST))){
 					Log.d(TAG_EIP_FRAGMENT, "onActivityResult() -> eipStop!");
 					eipStop();
-				}else{
+				} else{
 					Log.d(TAG_EIP_FRAGMENT, "onActivityResult() -> has no Extra STOP_FIRST");
 				}
 
+				ConfigHelper.saveSharedPref(EIP.PARSED_SERIAL, 0);
+				ConfigHelper.saveSharedPref(EIP.AUTHED_EIP, authed_eip);
 				startService( new Intent(EIP.ACTION_UPDATE_EIP_SERVICE) );
 				buildDashboard();
 				if(data != null && data.hasExtra(LogInDialog.VERB)) {
