af5268b24a546f9a7a2693ec3ae339dd6ca7ea4f
Parménides GV <parmegv@sdf.org>
2013-12-12 18:11:19 +0100

    About is now an Activity.
    I've also removed some spaces from the text.

---
+++ src/se/leap/bitmaskclient/AboutActivity.java
@@ -0,0 +1,48 @@
+package se.leap.bitmaskclient;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import se.leap.bitmaskclient.R;
+
+public class AboutActivity extends Activity  {
+	
+    final public static String TAG = "aboutFragment";
+    final public static int VIEWED = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.about);
+    	TextView ver = (TextView) findViewById(R.id.version);
+    	
+    	String version;
+    	String name="Openvpn";
+		try {
+			PackageInfo packageinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			version = packageinfo.versionName;
+			name = getString(R.string.app);
+		} catch (NameNotFoundException e) {
+			version = "error fetching version";
+		}
+
+    	
+    	ver.setText(getString(R.string.version_info,name,version));
+    	
+    	TextView translation = (TextView) findViewById(R.id.translation);
+    	
+    	// Don't print a text for myself
+    	if ( getString(R.string.translationby).contains("Arne Schwabe"))
+	    translation.setVisibility(TextView.INVISIBLE);
+	else
+	    translation.setText(R.string.translationby);
+	setResult(VIEWED);
+    }
+
+}
