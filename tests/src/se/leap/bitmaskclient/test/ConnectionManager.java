6c7eda789ac7ee5c0369409cb27430f0074dfc07
Parménides GV <parmegv@sdf.org>
2014-01-30 18:59:07 +0100

    Set data connection status works.
    Tested both in real device with Cyanogenmod 4.3.1 and in api 18
    emulator.
    
    Next step: wi-fi toggling.

--- tests/src/se/leap/bitmaskclient/test/ConnectionManager.java
+++ tests/src/se/leap/bitmaskclient/test/ConnectionManager.java
@@ -10,27 +10,18 @@ import android.util.Log;
 public class ConnectionManager {
     static void setMobileDataEnabled(boolean enabled, Context context) {
 	final ConnectivityManager conman = (ConnectivityManager)  context.getSystemService(Context.CONNECTIVITY_SERVICE);
-	try {
-	    final Class conmanClass = Class.forName(conman.getClass().getName());
-	    final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
-	    iConnectivityManagerField.setAccessible(true);
-	    final Object iConnectivityManager = iConnectivityManagerField.get(conman);
-	    final Class iConnectivityManagerClass =  Class.forName(iConnectivityManager.getClass().getName());
-	    final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
-	    setMobileDataEnabledMethod.setAccessible(true);
-
-	    setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
-	    Log.d("testDashboard", "setMobileDataEnabled(" + enabled + ");");
-	} catch (ClassNotFoundException e) {
-	    e.printStackTrace();
-	} catch (NoSuchMethodException e) {
-	    e.printStackTrace();
-	} catch (IllegalAccessException e) {
-	    e.printStackTrace();
-	} catch (NoSuchFieldException e) {
-	    e.printStackTrace();
-	} catch (InvocationTargetException e) {
-	    e.printStackTrace();
+	Method[] methods = conman.getClass().getMethods();
+	for (Method method : methods) {
+	    if (method.getName().equals("setMobileDataEnabled")) {
+		method.setAccessible(true);
+		try {
+		    method.invoke(conman, enabled);
+		} catch (InvocationTargetException e) {
+		    e.printStackTrace();
+		} catch (IllegalAccessException e) {
+		    e.printStackTrace();
+		}
+	    }
 	}
     }
 }
