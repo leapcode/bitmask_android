1f133c139e4f9f26a265b26cbb6f37c2556a5fcc
cyBerta <cyberta@riseup.net>
2017-10-29 18:18:38 +0100

    add missing import...

--- app/src/insecure/java/se/leap/bitmaskclient/ProviderAPI.java
+++ app/src/insecure/java/se/leap/bitmaskclient/ProviderAPI.java
@@ -56,6 +56,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import javax.net.ssl.HostnameVerifier;
