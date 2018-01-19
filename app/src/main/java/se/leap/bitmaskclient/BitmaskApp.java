7abdfa72e372d927bf2dec3185e05588cb1a94b7
cyBerta <cyberta@riseup.net>
2018-01-19 13:14:38 +0100

    #8822 adds leakCanary library to Bitmask for analyzing memory leaks

--- app/src/main/java/se/leap/bitmaskclient/BitmaskApp.java
+++ app/src/main/java/se/leap/bitmaskclient/BitmaskApp.java
@@ -1,6 +1,10 @@
 package se.leap.bitmaskclient;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 /**
  * Created by cyberta on 24.10.17.
@@ -8,11 +12,31 @@ import android.app.Application;
 
 public class BitmaskApp extends Application {
 
+    private RefWatcher refWatcher;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        refWatcher = LeakCanary.install(this);
+        // Normal app init code...*/
         PRNGFixes.apply();
-        //TODO: add LeakCanary!
     }
+
+    /**
+     * Use this method to get a RefWatcher object that checks for memory leaks in the given context.
+     * Call refWatcher.watch(this) to check if all references get garbage collected.
+     * @param context
+     * @return the RefWatcher object
+     */
+    public static RefWatcher getRefWatcher(Context context) {
+        BitmaskApp application = (BitmaskApp) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
 
 }
