1d3ecd7bb6cbb9ecac394cc494b095ef830dadee
cyBerta <cyberta@riseup.net>
2019-07-14 20:11:44 +0200

    remove equals method in Gateway class after rebasing onto latest master

--- app/src/main/java/se/leap/bitmaskclient/eip/Gateway.java
+++ app/src/main/java/se/leap/bitmaskclient/eip/Gateway.java
@@ -157,17 +157,4 @@ public class Gateway {
         return new Gson().toJson(this, Gateway.class);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof Gateway &&
-                (this.mVpnProfile != null &&
-                        ((Gateway) obj).mVpnProfile != null &&
-                        this.mVpnProfile.mConnections != null &&
-                        ((Gateway) obj).mVpnProfile != null &&
-                        this.mVpnProfile.mConnections.length > 0 &&
-                        ((Gateway) obj).mVpnProfile.mConnections.length > 0 &&
-                        this.mVpnProfile.mConnections[0].mServerName != null &&
-                        this.mVpnProfile.mConnections[0].mServerName.equals(((Gateway) obj).mVpnProfile.mConnections[0].mServerName)) ||
-                        this.mVpnProfile == null && ((Gateway) obj).mVpnProfile == null;
-    }
 }
