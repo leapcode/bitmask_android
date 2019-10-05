7776fd80c467ccc391bf657404467794b63c709b
cyBerta <cyberta@riseup.net>
2019-10-05 20:48:21 +0200

    improve golang installation script

--- go/install_go.sh
+++ go/install_go.sh
@@ -2,5 +2,16 @@
 
 GO_VERSION=go1.12.7.linux-amd64
 
-curl -o $GO_VERSION.tar.gz https://dl.google.com/go/$GO_VERSION.tar.gz
+if [[ $(ls -A ${GO_VERSION}.tar.gz) ]]
+then
+    echo "reusing downloaded golang bundle"
+else
+    echo "installing go lang bundle ${GO_VERSION}.tar.gz"
+    curl -o $GO_VERSION.tar.gz https://dl.google.com/go/$GO_VERSION.tar.gz
+fi
+
+if [[ $(ls -A ./golang/*) ]]
+then
+    rm -r ./golang/*
+fi
 tar -C ./golang -xzf $GO_VERSION.tar.gz
