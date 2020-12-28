16da1eeb5180cbb4a0d916785a08ccbcd3c1d74e
cyBerta <cyberta@riseup.net>
2020-12-28 23:20:02 +0100

    clean up pgpverify GO code

--- go/src/se.leap.bitmaskclient/pgpverify/pgpverify.go
+++ go/src/se.leap.bitmaskclient/pgpverify/pgpverify.go
@@ -9,9 +9,6 @@ import (
 
 // PgpVerifier - exported struct used for file verification
 type PgpVerifier struct {
-	//Signature string
-	//Target    string
-	//PublicKey string
 	Logger Logger
 }
 
@@ -46,37 +43,3 @@ func (pgpv *PgpVerifier) Verify(signature string, publicKey string, targetPath s
 	return true
 }
 
-/*func main() {
-	keyRingReader, err := os.Open("public_leap.asc")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	signature, err := os.Open("RiseupVPN_release_1.0.5.apk.sig")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	verificationTarget, err := os.Open("RiseupVPN_release_1.0.5.apk")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	keyring, err := openpgp.ReadArmoredKeyRing(keyRingReader)
-	if err != nil {
-		fmt.Println("Read Armored Key Ring: " + err.Error())
-		return
-	}
-	entity, err := openpgp.CheckArmoredDetachedSignature(keyring, verificationTarget, signature)
-	if err != nil {
-		fmt.Println("Check Detached Signature: " + err.Error())
-		return
-	} else {
-		fmt.Println("successfully verified")
-	}
-
-	fmt.Println(entity.Identities)
-}*/
