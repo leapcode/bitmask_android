cb1510c9f3ea27d5cffc0c0e370f8270d454e39b
cyBerta <cyberta@riseup.net>
2021-02-26 08:37:16 +0100

    adapt tests

--- app/src/test/java/se/leap/bitmaskclient/testutils/MockHelper.java
+++ app/src/test/java/se/leap/bitmaskclient/testutils/MockHelper.java
@@ -366,6 +366,8 @@ public class MockHelper {
                 return getClass().getClassLoader().getResourceAsStream(filename);
             }
         });
+        when(InputStreamHelper.extractKeyFromInputStream(any(InputStream.class), anyString())).thenCallRealMethod();
+        when(InputStreamHelper.inputStreamToJson(any(InputStream.class))).thenCallRealMethod();
 
     }
 
