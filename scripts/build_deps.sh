cdefcb0d109d4558d5b9531bde2178a8b9bad0dc
cyBerta <cyberta@riseup.net>
2021-07-09 04:56:56 +0200

    use bitmaskcore libs in android project

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -37,6 +37,18 @@ else
     then
         mkdir lib
     fi
-    ./build_core.sh
-    cd ..
+    ./build_core.sh || quit "failed to build bitmaskcore"
+    cp lib/bitmaskcore.aar ../lib-bitmask-core/.
+    cp lib/bitmaskcore-sources.jar ../lib-bitmask-core/.
+    cp lib/bitmaskcore_web.aar ../lib-bitmask-core-web/.
+    cp lib/bitmaskcore_web-sources.jar ../lib-bitmask-core-web/.
+    cp lib/bitmaskcore_arm.aar ../lib-bitmask-core-armv7/.
+    cp lib/bitmaskcore_arm-sources.jar ../lib-bitmask-core-armv7/.
+    cp lib/bitmaskcore_arm64.aar ../lib-bitmask-core-arm64/.
+    cp lib/bitmaskcore_arm64-sources.jar ../lib-bitmask-core-arm64/.
+    cp lib/bitmaskcore_x86.aar ../lib-bitmask-core-x86/.
+    cp lib/bitmaskcore_x86-sources.jar ../lib-bitmask-core-x86/.
+    cp lib/bitmaskcore_x86_64.aar ../lib-bitmask-core-x86_64/.
+    cp lib/bitmaskcore_x86_64-sources.jar ../lib-bitmask-core-x86_64/.
+
 fi
