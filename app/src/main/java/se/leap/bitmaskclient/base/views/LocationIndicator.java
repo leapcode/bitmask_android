96a397b01da596204e3e9d1ff2dd2c176f76e5b3
cyBerta <cyberta@riseup.net>
2021-11-15 20:42:58 +0100

    improve location status indicator, adapt according to desktop

--- app/src/main/java/se/leap/bitmaskclient/base/views/LocationIndicator.java
+++ app/src/main/java/se/leap/bitmaskclient/base/views/LocationIndicator.java
@@ -7,15 +7,21 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import se.leap.bitmaskclient.R;
 import se.leap.bitmaskclient.eip.GatewaysManager;
 
+import static androidx.core.content.ContextCompat.getColor;
+
 public class LocationIndicator extends LinearLayout {
 
     private View level1;
+    private View level1_2;
     private View level2;
+    private View level2_2;
     private View level3;
+    private View level3_2;
 
     public LocationIndicator(Context context) {
         super(context);
@@ -39,37 +45,46 @@ public class LocationIndicator extends LinearLayout {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rootview = inflater.inflate(R.layout.v_location_status_indicator, this, true);
         level1 = rootview.findViewById(R.id.level1);
+        level1_2 = rootview.findViewById(R.id.level1_2);
         level2 = rootview.findViewById(R.id.level2);
+        level2_2 = rootview.findViewById(R.id.level2_2);
         level3 = rootview.findViewById(R.id.level3);
+        level3_2 = rootview.findViewById(R.id.level3_2);
     }
 
     public void setLoad(GatewaysManager.Load load) {
         switch (load) {
             case GOOD:
-                level1.setBackgroundColor(getResources().getColor(R.color.green200));
-                level2.setBackgroundColor(getResources().getColor(R.color.green200));
-                level3.setBackgroundColor(getResources().getColor(R.color.green200));
-                level1.setVisibility(VISIBLE);
-                level2.setVisibility(VISIBLE);
-                level3.setVisibility(VISIBLE);
+                level1.setBackgroundColor(getColor(getContext(), R.color.green200));
+                level1_2.setBackgroundColor(getColor(getContext(), R.color.green200));
+                level2.setBackgroundColor(getColor(getContext(), R.color.green200));
+                level2_2.setBackgroundColor(getColor(getContext(), R.color.green200));
+                level3.setBackgroundColor(getColor(getContext(), R.color.green200));
+                level3_2.setBackgroundColor(getColor(getContext(), R.color.green200));
                 break;
             case AVERAGE:
-                level1.setBackgroundColor(getResources().getColor(R.color.yellow200));
-                level2.setBackgroundColor(getResources().getColor(R.color.yellow200));
-                level1.setVisibility(VISIBLE);
-                level2.setVisibility(VISIBLE);
-                level3.setVisibility(INVISIBLE);
+                level1.setBackgroundColor(getColor(getContext(), R.color.yellow200));
+                level1_2.setBackgroundColor(getColor(getContext(), R.color.yellow200));
+                level2.setBackgroundColor(getColor(getContext(), R.color.yellow200));
+                level2_2.setBackgroundColor(getColor(getContext(), R.color.yellow200));
+                level3.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level3_2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
                 break;
             case CRITICAL:
-                level1.setBackgroundColor(getResources().getColor(R.color.red200));
-                level1.setVisibility(VISIBLE);
-                level2.setVisibility(INVISIBLE);
-                level3.setVisibility(INVISIBLE);
+                level1.setBackgroundColor(getColor(getContext(), R.color.red200));
+                level1_2.setBackgroundColor(getColor(getContext(), R.color.red200));
+                level2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level2_2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level3.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level3_2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
                 break;
             default:
-                level1.setVisibility(INVISIBLE);
-                level2.setVisibility(INVISIBLE);
-                level3.setVisibility(INVISIBLE);
+                level1.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level1_2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level2_2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level3.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));
+                level3_2.setBackgroundColor(getColor(getContext(), R.color.black800_high_transparent));;
                 break;
         }
     }
