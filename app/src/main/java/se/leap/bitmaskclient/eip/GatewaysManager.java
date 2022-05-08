cb35788e761417a98a9660c9f5c99acadf6d719d
kwadronaut <kwadronaut@leap.se>
2022-05-08 15:17:12 +0200

    update min l110n, (c) date

--- app/src/main/java/se/leap/bitmaskclient/eip/GatewaysManager.java
+++ app/src/main/java/se/leap/bitmaskclient/eip/GatewaysManager.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 - 2019 LEAP Encryption Access Project and contributors
+ * Copyright (c) 2013 - 2022 LEAP Encryption Access Project and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
