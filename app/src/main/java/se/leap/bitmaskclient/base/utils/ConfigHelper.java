a8cbf6ea3c794e6cad818979a9c2d4ac95fb7a0f
cyBerta <cyberta@riseup.net>
2024-01-25 23:24:23 +0100

    remove methods from ConfigHelper after moving functionality to CertificateHelper

--- app/src/main/java/se/leap/bitmaskclient/base/utils/ConfigHelper.java
+++ app/src/main/java/se/leap/bitmaskclient/base/utils/ConfigHelper.java
@@ -34,9 +34,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
@@ -121,32 +118,6 @@ public class ConfigHelper {
         }
 
         return null;
-    }
-
-    private static String byteArrayToHex(byte[] input) {
-        int readBytes = input.length;
-        StringBuffer hexData = new StringBuffer();
-        int onebyte;
-        for (int i = 0; i < readBytes; i++) {
-            onebyte = ((0x000000ff & input[i]) | 0xffffff00);
-            hexData.append(Integer.toHexString(onebyte).substring(6));
-        }
-        return hexData.toString();
-    }
-
-    /**
-     * Calculates the hexadecimal representation of a sha256/sha1 fingerprint of a certificate
-     *
-     * @param certificate
-     * @param encoding
-     * @return
-     * @throws NoSuchAlgorithmException
-     * @throws CertificateEncodingException
-     */
-    @NonNull
-    public static String getFingerprintFromCertificate(X509Certificate certificate, String encoding) throws NoSuchAlgorithmException, CertificateEncodingException /*, UnsupportedEncodingException*/ {
-        byte[] byteArray = MessageDigest.getInstance(encoding).digest(certificate.getEncoded());
-        return byteArrayToHex(byteArray);
     }
 
     public static void ensureNotOnMainThread(@NonNull Context context) throws IllegalStateException{
