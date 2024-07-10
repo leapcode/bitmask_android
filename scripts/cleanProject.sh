f65aea3ef8c0d784db9797206ed66b8438a01365
cyBerta <cyberta@riseup.net>
2024-07-10 11:08:49 +0200

    delete tor-android directory and re-fetch it

--- scripts/cleanProject.sh
+++ scripts/cleanProject.sh
@@ -11,15 +11,16 @@ git submodule foreach --recursive git clean -xfd
 rm -r $BASE_DIR/ics-openvpn
 rm -r $BASE_DIR/build
 rm -r $BASE_DIR/app/build
-rm -r $BASE_DIR/bitmaskcore/lib/*
-rm -r $BASE_DIR/bitmaskcore/golang/
+rm -r $BASE_DIR/bitmask-core-android/lib/*
+rm -r $BASE_DIR/bitmask-core-android/golang/
 rm -r $BASE_DIR/currentReleases
-rm -r $BASE_DIR/tor-android/build
-rm -r $BASE_DIR/tor-android/tor-android-binary/build
-rm -r $BASE_DIR/tor-android/external/bin
-rm -r $BASE_DIR/tor-android/external/include/
-rm -r $BASE_DIR/tor-android/external/*.build-stamp
-rm -r $BASE_DIR/tor-android/external/lib
+#rm -r $BASE_DIR/tor-android/build
+#rm -r $BASE_DIR/tor-android/tor-android-binary/build
+#rm -r $BASE_DIR/tor-android/external/bin
+#rm -r $BASE_DIR/tor-android/external/include/
+#rm -r $BASE_DIR/tor-android/external/*.build-stamp
+#rm -r $BASE_DIR/tor-android/external/lib
+rm -r $BASE_DIR/tor-android
 git reset --hard
 git submodule foreach --recursive git reset --hard HEAD
 git submodule sync --recursive
