df47b150a7a04b3d85751ee2ef05da4bd488f0fd
cyberta <cyberta@riseup.net>
2026-06-26 17:47:12 +0200

    update copyright years

--- app/src/main/java/se/leap/bitmaskclient/base/fragments/AboutFragment.java
+++ app/src/main/java/se/leap/bitmaskclient/base/fragments/AboutFragment.java
@@ -11,8 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import se.leap.bitmaskclient.BuildConfig;
